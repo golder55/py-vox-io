64969b5051b70834a56e4be5640c60dd44794b9e
golder55 <57981038+golder55@users.noreply.github.com>
2021-08-17 10:38:18 +0530

    Update parser.py

--- pyvox/parser.py
+++ pyvox/parser.py
@@ -5,11 +5,15 @@ from .models import Vox, Size, Voxel, Color, Model, Material
 
 log = logging.getLogger(__name__)
 
-class ParsingException(Exception): pass
+
+class ParsingException(Exception):
+    pass
+
 
 def bit(val, offset):
     mask = 1 << offset
     return(val & mask)
+
 
 class Chunk(object):
     def __init__(self, id, content=None, chunks=None):
@@ -18,7 +22,8 @@ class Chunk(object):
         self.chunks = chunks or []
 
         if id == b'MAIN':
-            if len(self.content): raise ParsingException('Non-empty content for main chunk')
+            if len(self.content):
+                raise ParsingException('Non-empty content for main chunk')
         elif id == b'PACK':
             self.models = unpack('i', content)[0]
         elif id == b'SIZE':
@@ -27,9 +32,11 @@ class Chunk(object):
             n = unpack('i', content)[0]
             log.debug('xyzi block with %d voxels (len %d)', n, len(content))
             self.voxels = []
-            self.voxels = [ Voxel(*unpack('BBBB', content, 4+4*i)) for i in range(n) ]
+            self.voxels = [Voxel(*unpack('BBBB', content, 4+4*i))
+                           for i in range(n)]
         elif id == b'RGBA':
-            self.palette = [ Color(*unpack('BBBB', content, 4*i)) for i in range(255) ]
+            self.palette = [Color(*unpack('BBBB', content, 4*i))
+                            for i in range(255)]
             # Docs say:  color [0-254] are mapped to palette index [1-255]
             # hmm
             # self.palette = [ Color(0,0,0,0) ] + [ Color(*unpack('BBBB', content, 4*i)) for i in range(255) ]
@@ -37,22 +44,23 @@ class Chunk(object):
             _id, _type, weight, flags = unpack('iifi', content)
             props = {}
             offset = 16
-            for b,field in [ (0, 'plastic'),
+            for b, field in [(0, 'plastic'),
                              (1, 'roughness'),
                              (2, 'specular'),
                              (3, 'IOR'),
                              (4, 'attenuation'),
                              (5, 'power'),
                              (6, 'glow'),
-                             (7, 'isTotalPower') ]:
-                if bit(flags, b) and b<7: # no value for 7 / isTotalPower
+                             (7, 'isTotalPower')]:
+                if bit(flags, b) and b < 7:  # no value for 7 / isTotalPower
                     props[field] = unpack('f', content, offset)
                     offset += 4
 
             self.material = Material(_id, _type, weight, props)
 
         else:
-            raise ParsingException('Unknown chunk type: %s'%self.id)
+            pass  # raise ParsingException('Unknown chunk type: %s'%self.id)
+
 
 class VoxParser(object):
 
@@ -73,55 +81,58 @@ class VoxParser(object):
 
         log.debug("Found chunk id %s / len %s / children %s", _id, N, M)
 
-        content = self.unpack('%ds'%N)[0]
+        content = self.unpack('%ds' % N)[0]
 
         start = self.offset
-        chunks = [ ]
-        while self.offset<start+M:
+        chunks = []
+        while self.offset < start+M:
             chunks.append(self._parseChunk())
 
         return Chunk(_id, content, chunks)
 
     def parse(self):
 
-            header, version = self.unpack('4si')
+        header, version = self.unpack('4si')
 
-            if header != b'VOX ': raise ParsingException("This doesn't look like a vox file to me")
+        if header != b'VOX ':
+            raise ParsingException("This doesn't look like a vox file to me")
 
-            if version != 150: raise ParsingException("Unknown vox version: %s expected 150"%version)
+        if version != 150:
+            raise ParsingException(
+                "Unknown vox version: %s expected 150" % version)
 
-            main = self._parseChunk()
+        main = self._parseChunk()
 
-            if main.id != b'MAIN': raise ParsingException("Missing MAIN Chunk")
+        if main.id != b'MAIN':
+            raise ParsingException("Missing MAIN Chunk")
 
-            chunks = list(reversed(main.chunks))
-            if chunks[-1].id == b'PACK':
-                models = chunks.pop().models
-            else:
-                models = 1
+        chunks = list(reversed(main.chunks))
+        if chunks[-1].id == b'PACK':
+            models = chunks.pop().models
+        else:
+            models = 1
 
-            log.debug("file has %d models", models)
+        log.debug("file has %d models", models)
 
-            models = [ self._parseModel(chunks.pop(), chunks.pop()) for _ in range(models) ]
+        models = [self._parseModel(chunks.pop(), chunks.pop())
+                  for _ in range(models)]
 
-            if chunks and chunks[0].id == b'RGBA':
-                palette = chunks.pop().palette
-            else:
-                palette = None
+        if chunks and chunks[0].id == b'RGBA':
+            palette = chunks.pop().palette
+        else:
+            palette = None
 
-            materials = [ c.material for c in chunks ]
+        materials = []  # [ c.material for c in chunks ]
 
-            return Vox(models, palette, materials)
-
-
+        return Vox(models, palette, materials)
 
     def _parseModel(self, size, xyzi):
-        if size.id != b'SIZE': raise ParsingException('Expected SIZE chunk, got %s', size.id)
-        if xyzi.id != b'XYZI': raise ParsingException('Expected XYZI chunk, got %s', xyzi.id)
+        if size.id != b'SIZE':
+            raise ParsingException('Expected SIZE chunk, got %s', size.id)
+        if xyzi.id != b'XYZI':
+            raise ParsingException('Expected XYZI chunk, got %s', xyzi.id)
 
         return Model(size.size, xyzi.voxels)
-
-
 
 
 if __name__ == '__main__':
@@ -130,6 +141,5 @@ if __name__ == '__main__':
     import coloredlogs
 
     coloredlogs.install(level=logging.DEBUG)
-
 
     VoxParser(sys.argv[1]).parse()
